b322be7371b7b14a8fac98fcdebf58621524bb99
Morten W. Hansen <mortenwh@met.no>
2025-01-28 11:47:39 +0000

    import module

--- sar_doppler/management/commands/fix_geospatial.py
+++ sar_doppler/management/commands/fix_geospatial.py
@@ -8,6 +8,7 @@ from multiprocessing import Pool
 
 from nansat.nansat import Nansat
 
+from django.db import connection
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
